fdbbb8d6ee26d64aec221f59de8407efbe59c55d
Long Phạm <Longpham30500@gmail.com>
2022-09-29 19:50:06 +0700

    Profile

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import Avatar from './Avatar'
+import React from "react";
+import Avatar from "./Avatar";
+import { Link } from "react-router-dom";
 
-const UserCard = ({user, border}) => {
+const UserCard = ({ user, border, handleClose }) => {
+
+  const handleCloseAll = () => {
+    if(handleClose) handleClose()
+  }
+
   return (
     <div className={`d-flex p-2 align-item-center ${border}`}>
-        <Avatar src={user.avatar} size="big-avatar" />
-        <div className="m1-1" style={{transform: 'translateY(-2px)'}}>
-            <span className= "d-block">{user.username}</span>
-            <small style={{opacity: 0.7}}>{user.fullname}</small>
-        </div>
-    </div>
-  )
-}
+      <div>
+        <Link to={`/profile/${user._id}`} onClick={handleCloseAll}
+        className="d-flex align-item-center">
 
-export default UserCard
+          <Avatar src={user.avatar} size="big-avatar" />
+          
+          <div className="m1-1" style={{ transform: "translateY(-2px)" }}>
+            <span className="d-block">{user.username}</span>
+            <small style={{ opacity: 0.7 }}>{user.fullname}</small>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
--- client/src/components/header/Search.js
+++ client/src/components/header/Search.js
@@ -1,63 +1,82 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch} from 'react-redux'
-import { getDataAPI } from '../../utils/fetchData'
-import { GLOBALTYPES } from '../../redux/actions/globalTypes'
-import { Link } from 'react-router-dom'
-import UserCard from '../UserCard'
+import React, { useState, } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getDataAPI } from "../../utils/fetchData";
+import { GLOBALTYPES } from "../../redux/actions/globalTypes";
+import UserCard from "../UserCard";
+import LoadIcon from '../../images/loading.gif'
 
 const Search = () => {
-    const [search, setSearch] = useState('')
-    const [users, setUsers] = useState([])
+  const [search, setSearch] = useState("");
+  const [users, setUsers] = useState([]);
 
-    const { auth } = useSelector(state => state)
-    const dispatch = useDispatch  ()
+  const { auth } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const [load, setLoad] = useState(false)
 
-    useEffect(() => {
-      if(search){
-        getDataAPI(`search?username=${search}`, auth.token)
-        .then(res => setUsers(res.data.users))
-        .catch(err => {
-            dispatch({
-              type: GLOBALTYPES.ALERT, payload: {err: err.response.data.msg}
-            })
-        })
-      }else{
-        setUsers([])
-      }
+ 
 
-    },[search, auth.token, dispatch])
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!search) return;
 
-    const handleClose = () => {
-      setSearch('')
-      setUsers([])
+    try {
+      setLoad(true)
+      const res = await getDataAPI(`search?username=${search}`, auth.token);
+      setUsers(res.data.users)
+      setLoad(false)
+    } catch (err) {
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: { err: err.response.data.msg },
+      });
     }
+  };
+
+  const handleClose = () => {
+    setSearch("");
+    setUsers([]);
+  };
 
   return (
-    <form className="search_form">
-        <input type="text" name="search" value={search} id="search"
-        onChange={e => setSearch(e.target.value.toLowerCase().replace(/ /g, ''))}/>
+    <form className="search_form" onSubmit={handleSearch}>
+      <input
+        type="text"
+        name="search"
+        value={search}
+        id="search"
+        onChange={(e) =>
+          setSearch(e.target.value.toLowerCase().replace(/ /g, ""))
+        }
+      />
 
-        <div className="search_icon" style={{opacity: search ? 0 : 0.3}}>
-            <span className="material-icons">search</span>
-            <span>search</span>
-        </div>
+      <div className="search_icon" style={{ opacity: search ? 0 : 0.3 }}>
+        <span className="material-icons">search</span>
+        <span>search</span>
+      </div>
 
-        <div className="close_search" onClick={handleClose} 
-          style={{opacity: users.length === 0 ? 0 : 1}}>
-          &times;
-        </div>
+      <div
+        className="close_search"
+        onClick={handleClose}
+        style={{ opacity: users.length === 0 ? 0 : 1 }}
+      >
+        &times;
+      </div>
 
-        <div className="users">
-          {
-            search && users.map(user => (
-              <Link key={user._id} to={`/profile/${user._id}`} onClick={handleClose}>
-                  <UserCard user={user} border="border" />
-              </Link>
-            ))
-          }
-        </div> 
+      <button type="submit" style={{display: 'none'}}>Search</button>
+      
+      { load && <img className="loading" src={LoadIcon} alt="loading" /> }
+      <div className="users">
+        {search && users.map(user => (
+              <UserCard 
+              key={user._id} 
+              user={user} 
+              border="border"
+              handleClose={handleClose}
+              />
+          ))}
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
