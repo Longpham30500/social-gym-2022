949863a932ced6eb134914e5eff78568ec4c5be0
Long Phạm <Longpham30500@gmail.com>
2022-10-08 15:21:57 +0700

    UI stream camera and add images(create post)

--- client/src/components/StatusModal.js
+++ client/src/components/StatusModal.js
@@ -1,13 +1,79 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { GLOBALTYPES } from "../redux/actions/globalTypes";
 import "boxicons";
 const StatusModal = () => {
-  const { auth } = useSelector((state) => state);
+  const { auth, theme } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const [content, setContent] = useState("");
+  const [images, setImages] = useState([])
+
+  const [stream, setStream] = useState(false)
+  const videoRef = useRef()
+  const refCanvas = useRef()
+  const [tracks, setTracks] = useState('')
+
+
+  const handleChangeImages = e => {
+    const files = [...e.target.files]
+    let err = ""
+    let newImages = []
+
+    files.forEach(file => {
+      if(!file) return err = "file does not exist."
+
+      if(file.type !== 'image/jpeg' && file.type !== 'image/png'){
+        return err = "Image format is incorrect."
+      }
+
+      return newImages.push(file)
+    })
+
+    if(err) dispatch({ type: GLOBALTYPES.ALERT, payload: {err: err} })
+    setImages([...images,...newImages])
+  }
+
+  const deleteImages = (index) => {
+    const newArr = [...images]
+    newArr.splice(index, 1)
+    setImages(newArr)
+  }
+
+  const handleStream = () => {
+    setStream(true)
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+      navigator.mediaDevices.getUserMedia({video:true})
+      .then(mediaStream => {
+        videoRef.current.srcObject = mediaStream
+        videoRef.current.play()
+
+        const track = mediaStream.getTracks()
+        setTracks(track[0])
+      }).catch(err => console.log(err))
+    }
+  }
+
+  const handleCapture = () => {
+    const width = videoRef.current.clientWidth;
+    const height = videoRef.current.clientHeight;
+
+    refCanvas.current.setAttribute("width", width)
+    refCanvas.current.setAttribute("height", height)
+
+    const ctx = refCanvas.current.getContext('2d')
+    ctx.drawImage(videoRef.current, 0, 0, width, height)
+    let URL = refCanvas.current.toDataURL()
+    setImages([...images, {camera: URL}])
+
+
+  }
+
+  const handleStopStream = () => {
+    tracks.stop()
+    setStream(false)
+  }
+
   return (
     <div className="status_modal">
       <form>
@@ -30,20 +96,51 @@ const StatusModal = () => {
             name="content"
             value={content}
             placeholder={`${auth.user.username}, what are you thinking?`}
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <div className="input_images">
-                <box-icon type='solid' name='camera'></box-icon>
-            
-                <div className="file_upload">
-                    <box-icon type="solid" name="file-image" />
-                    <input
-                        type="file"
-                        name="file"
-                        id="file"
-                        multiple
-                        accept="image/*" />           
-                </div>
+            onChange={(e) => setContent(e.target.value)} />
+
+            <div className="show_images">
+              {
+                images.map((img, index) => (
+                  <div key={index} id="file_img">
+                    <img src={img.camera ? img.camera : URL.createObjectURL(img)}
+                    alt="images" className="img-thumbnail"
+                    style={{filter: theme ? 'invert(1)' : 'invert(0)'}} />
+                    <span onClick={() => deleteImages(index)}>&times;</span>
+                  </div>
+                ))
+              }
+            </div>
+
+            {
+              stream &&
+              <div className="stream position-relative">
+                <video autoPlay muted ref={videoRef} width="100%" height="100%"
+                style={{filter: theme ? 'invert(1)' : 'invert(0)'}} />
+
+                <span onClick={handleStopStream}>&times;</span>
+                <canvas ref={refCanvas} style={{display: 'none'}} />
+              </div>
+            }
+
+            <div className="input_images">
+              {
+                stream
+                ? <box-icon type='solid' name='camera' onClick={handleCapture} />
+                : <>
+                    <box-icon type='solid' name='camera' onClick={handleStream} />
+                
+                    <div className="file_upload">
+                        <box-icon type="solid" name="file-image" />
+                        <input
+                            type="file"
+                            name="file"
+                            id="file"
+                            multiple
+                            accept="image/*" onChange={handleChangeImages} />           
+                    </div>
+                  </>
+              }
+                
           </div>
         </div>
 
