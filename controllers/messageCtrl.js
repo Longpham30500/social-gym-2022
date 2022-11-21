40f92fa7efc7ab0753df89ce75bc2dc7e01fb5a7
longpham <Longpham30500@gmail.com>
2022-11-21 23:23:06 +0700

    get message with id

--- controllers/messageCtrl.js
+++ controllers/messageCtrl.js
@@ -1,75 +1,98 @@
-
-
-const ModelConversation = require('../models/conversationModel')
-const ModelMessages = require('../models/messageModel')
+const ModelConversation = require("../models/conversationModel");
+const ModelMessages = require("../models/messageModel");
 
 class Messfeatures {
-    constructor(query, queryString){
-        this.query = query
-        this.queryString = queryString
-    }
-    paginating(){
-        const page = this.queryString.page * 1 || 1
-        const limit = this.queryString.limit * 1 || 6
-        const skip = (page - 1) * limit
-        this.query = this.query.skip(skip).limit(limit)
-        return this
-    }
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+  paginating() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 6;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
 }
 const messageCtrl = {
-    createMessage: async (req, res) => {
-        try {
-            const {text, recipient, media } = req.body
+  createMessage: async (req, res) => {
+    try {
+      const { text, recipient, media } = req.body;
 
-            if(!recipient || (!text.trim() && media.length === 0)) return;
+      if (!recipient || (!text.trim() && media.length === 0)) return;
 
-            const new_conversation = await ModelConversation.findOneAndUpdate({
-                    $or: [
-                        {recipients: [req.user._id, recipient]},
-                        {recipients: [recipient, req.user._id]}
-                    ]
-                }, {
-                recipients: [req.user._id, recipient],
-                    text, media
-                }, { new: true,upsert: true })
+      const new_conversation = await ModelConversation.findOneAndUpdate(
+        {
+          $or: [
+            { recipients: [req.user._id, recipient] },
+            { recipients: [recipient, req.user._id] },
+          ],
+        },
+        {
+          recipients: [req.user._id, recipient],
+          text,
+          media,
+        },
+        { new: true, upsert: true }
+      );
 
-            const new_message = new ModelMessages({
-                conversation: new_conversation._id,
-                sender: req.user._id,
-                recipient, text, media
-            })
+      const new_message = new ModelMessages({
+        conversation: new_conversation._id,
+        sender: req.user._id,
+        recipient,
+        text,
+        media,
+      });
 
-            await new_message.save()
+      await new_message.save();
 
-            res.json({msg: 'Create message success !'})
+      res.json({ msg: "Create message success !" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
+  getMessages: async (req, res) => {
+    try {
+      const features = new Messfeatures(
+        ModelMessages.find({
+          $or: [
+            { sender: req.user._id, recipient: req.params.id },
+            { sender: req.params.id, recipient: req.user._id },
+          ],
+        }),
+        req.query
+      ).paginating();
 
-    getConversations: async (req, res) => {
-        try {   
-            
-            const features = new Messfeatures(ModelConversation.find({
-                    recipients: req.user._id
-                }), req.query).paginating()
+      const messages = await features.query.sort("-createdAt");
 
-            const conversations = await features.query.sort('-updatedAt')
-            .populate('recipients', 'avatar name')
+      res.json({
+        messages,
+        result: messages.length,
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 
+  getConversations: async (req, res) => {
+    try {
+      const features = new Messfeatures(
+        ModelConversation.find({
+          recipients: req.user._id,
+        }),
+        req.query
+      ).paginating();
 
-            res.json({conversations,
-                      result: conversations.length
-                    })
+      const conversations = await features.query
+        .sort("-updatedAt")
+        .populate("recipients", "avatar name");
 
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
+      res.json({ conversations, result: conversations.length });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
 
-
-
-}
-
-module.exports = messageCtrl
+module.exports = messageCtrl;
